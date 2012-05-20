e2e67572bf92cee5ebf215c6ccec0ed50f9b2c7f
Rieethaa <rieethaa@gmail.com>
2012-05-20 22:37:00 +0100

    added a couple of simple specs

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,22 @@
 class AnagramFinder
-  
+  attr_accessor :dictionary
+
+  def get_anagrams(word)
+    # load the dictionary if it has not been loaded already (read from file)
+    sorted_word = sort_word(word)
+    dictionary[sorted_word].reject { |x| x == word }
+  end
+
+  def sort_word(word)
+    word.chars.sort.join
+  end
+
+  # Implement a method that loads the dictionary if its not already loaded
+  # 1. Check whether its already loaded
+  # 2. Get an array of words from the WordListReader
+  # 3. Iterate the array of words and put each one into the dictionary
+end
+
+class WordListReader
+  # Implements a method that returns an array of words given a filename
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,9 +1,32 @@
 require 'rspec'
 require_relative '../lib/anagram'
 
-describe Anagram do
+describe AnagramFinder do
   it 'should find anagrams' do
     finder = AnagramFinder.new
+    finder.dictionary = { 'ikln' => ['link', 'kiln'] }
     finder.get_anagrams('link').should == ['kiln']
+    finder.get_anagrams('link').should include 'kiln'
+  end
+
+  it 'should find anagrams when the dictionary is not initialised' do
+    finder = AnagramFinder.new
+    finder.get_anagrams('link').should == ['kiln']
+    finder.get_anagrams('link').should include 'kiln'
+  end
+
+  it 'should return sorted word for a given word' do
+    finder = AnagramFinder.new
+    finder.sort_word('doctor').should == 'cdoort'
+  end
+
+
+end
+
+describe WordListReader do
+
+  it 'should open wordlist.txt and return each word in the file' do
+    pending
+    reader = WordListReader.new
   end
 end
